ea3152ac7b904fe7fe983ed9b895be52ac303866
nhatnv77 <nhatnv77@gmail.com>
2021-10-21 17:37:36 +0700

    update

--- src/ptithcm/controller/CartController.java
+++ src/ptithcm/controller/CartController.java
@@ -5,10 +5,8 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
-import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
